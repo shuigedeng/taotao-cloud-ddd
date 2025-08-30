ee0ff85ba316aa4d018d05963b2844bb95c880ff
shuigedeng <981376577@qq.com>
2025-08-30 17:09:22 +0800

    开启2025.10

--- taotao-cloud-ddd-interfaces/src/main/java/com/taotao/cloud/ddd/interfaces/feign/DictApiImpl.java
+++ taotao-cloud-ddd-interfaces/src/main/java/com/taotao/cloud/ddd/interfaces/feign/DictApiImpl.java
@@ -19,8 +19,8 @@ package com.taotao.cloud.ddd.interfaces.feign;
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.taotao.boot.common.utils.log.LogUtils;
 import com.taotao.boot.idempotent.annotation.Idempotent;
-import com.taotao.boot.ratelimit.guava.GuavaLimit;
-import com.taotao.boot.ratelimit.guava.Limit;
+
+
 import com.taotao.boot.security.spring.annotation.NotAuth;
 import com.taotao.boot.web.request.annotation.RequestLogger;
 import com.taotao.boot.webagg.controller.FeignController;
