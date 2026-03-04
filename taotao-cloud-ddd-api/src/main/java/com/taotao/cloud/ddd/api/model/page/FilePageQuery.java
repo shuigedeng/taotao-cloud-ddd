e975e781cdc02fcb51985c81f09f26b6b52345a8
shuigedeng <981376577@qq.com>
2026-03-04 16:39:14 +0800

    开启2026.03

--- taotao-cloud-ddd-api/src/main/java/com/taotao/cloud/ddd/api/model/page/FilePageQuery.java
+++ taotao-cloud-ddd-api/src/main/java/com/taotao/cloud/ddd/api/model/page/FilePageQuery.java
@@ -16,7 +16,7 @@
 
 package com.taotao.cloud.ddd.api.model.page;
 
-import com.taotao.boot.common.model.request.PageQuery;
+import com.taotao.boot.common.model.ddd.query.PageQuery;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.*;
